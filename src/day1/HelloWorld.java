3918d59db44403dc7370bf2e1e53f31033f7a904
JC <qaderi.bizhan@gmail.com>
2024-02-19 14:21:51 -0500

    Hello World updated

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 
         System.out.println("How are you today? ");
         System.out.println("I am fine ");
+        System.out.println("Hi, I am not fine today");
 
 
 
