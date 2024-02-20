f6e4291c6e41bdfe37accfb78c9340315768b683
JC <qaderi.bizhan@gmail.com>
2024-02-19 22:00:32 -0500

    bizhan1.java is added to git

---
+++ src/day2/bizhan1.java
@@ -0,0 +1,11 @@
+package day2;
+
+public class bizhan1 {
+    public static void main(String[] args) {
+        System.out.println("How are you Bizhan? ");
+
+        System.out.println("Bizhan answered: I am fine. ");
+
+
+    }
+}
